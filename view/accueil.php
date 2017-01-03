f2229a1ac1c0bcb75ecc5c55d6bdd10fd2ec469c
afalcozvigne <a.falcozvigne@epsi.fr>
2017-01-03 13:41:01 +0100

    push Accueil

--- view/accueil.php
+++ view/accueil.php
@@ -5,6 +5,23 @@
  * Date: 03/01/2017
  * Time: 10:27
  */
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+    <?php include ("../include/head.php")?>
+</head>
+    <div class="row">
+        <div class="col-md-1">
 
+        </div>
+        <div class="col-md-10">
+            <?php include ("../include/carrousel.php")?>
+        </div>
+        <div class="col-md-1">
 
+        </div>
+    </div>
 
+</body>
+</html>
