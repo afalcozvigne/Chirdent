50bd80a53aabb3bdeda9cdb2d0b0077c07946f30
afalcozvigne <a.falcozvigne@epsi.fr>
2017-01-03 14:58:32 +0100

    sdfsfdsfs

--- include/head.php
+++ include/head.php
@@ -1,7 +1,8 @@
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width">
     <title>Occasion Dentaire</title>
+    <link rel="stylesheet" href="../css/navbar-twitch.css">
     <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/media.css">
--- view/accueil.php
+++ view/accueil.php
@@ -11,17 +11,20 @@
 <head>
     <?php include ("../include/head.php")?>
 </head>
+<body>
+<div class="container"
     <div class="row">
-        <div class="col-md-1">
+        <div class="row">
+            <div class="col-md-1">
+                <?php include ("../include/sideNavbar.php");?>
+            </div>
+            <div class="col-md-10">
+                <?php include ("../include/carrousel.php")?>
+            </div>
+            <div class="col-md-1">
 
-        </div>
-        <div class="col-md-10">
-            <?php include ("../include/carrousel.php")?>
-        </div>
-        <div class="col-md-1">
-
+            </div>
         </div>
     </div>
-
 </body>
 </html>
